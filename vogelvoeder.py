d0a205d7863026193bf40562999c998460b3929a
Ziyaad <Ziyaadwa1@gmail.com>
2022-12-09 15:09:13 +0100

    rj

--- vogelvoeder.py
+++ vogelvoeder.py
@@ -1,7 +1,7 @@
-from gpiozero import Servo,MotionSensor
+from gpiozero import Servo,MotionSensor,DistanceSensor
 from time import sleep
 
-
+sensor = DistanceSensor(18,24)
 pir = MotionSensor(23)
 teller = 0
 myGPIO=17
@@ -11,7 +11,7 @@ vorige_status = False
 
 def teller1(teller):
     teller+=1
-    print(teller)
+    print(f"Het aantal vogels is: {teller}")
     return teller
 
 def servo1():
@@ -21,6 +21,12 @@ def servo1():
     servo.max()
     print('open')
     sleep(2)
+
+def distance(afstand):
+    afstand = sensor.distance * 30
+    print(f"{afstand} cm")
+    return afstand
+
 
 
 while True:
